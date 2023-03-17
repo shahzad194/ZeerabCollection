4cf14ecb494ec2cf86469c294d5a898107a904a9
shahzad Aslam <Shahzadaslam7424@gmail.com>
2023-03-17 19:06:12 +0500

    First Commit

---
+++ ZeerabCollection.Web/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="ZeerabCollection.Web.MvcApplication" Language="C#" %>
---
+++ ZeerabCollection.Web/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ZeerabCollection.Web.Startup))]
+namespace ZeerabCollection.Web
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
