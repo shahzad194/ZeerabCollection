84bc3b002318c68907dd08d1cb4f5c30589b247c
shahzad Aslam <Shahzadaslam7424@gmail.com>
2023-03-17 20:43:12 +0500

    Added entities,database,and enable migration,add migration,and update database

---
+++ ZeerabCollection.Database/ZRContext.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ZeerabCollection.Entities;
+
+namespace ZeerabCollection.Database
+{
+    public class ZRContext : DbContext
+    {
+        public ZRContext() : base("ZeerabCollectionConnection")
+        {
+
+        }
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Product> Products { get; set; }
+    }
+}
---
+++ ZeerabCollection.Entities/Product.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ZeerabCollection.Entities
+{
+    public class Product : BaseEntity
+    {
+        public Category Category { get; set; }
+        public decimal Price { get; set; }
+    }
+}
