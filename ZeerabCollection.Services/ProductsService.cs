dfc8d20828fb4420c52f0880be1e991f39d8f84e
shahzad Aslam <Shahzadaslam7424@gmail.com>
2023-03-18 18:20:11 +0500

    perform Product Service and apply jquery ajax

---
+++ ZeerabCollection.Services/ProductsService.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ZeerabCollection.Database;
+using ZeerabCollection.Entities;
+
+namespace ZeerabCollection.Services
+{
+    public class ProductsService
+    {
+        public Product GetProduct(int id)
+        {
+            using (var context = new ZRContext())
+            {
+                return context.Products.Find(id);
+            }
+        }
+        public List<Product> GetProducts()
+        {
+            using (var context = new ZRContext())
+            {
+                return context.Products.ToList();
+            }
+        }
+        public void SaveProduct(Product product)
+        {
+            using (var context = new ZRContext())
+            {
+                context.Products.Add(product);
+                context.SaveChanges();
+            }
+        }
+        public void UpdateProduct(Product product)
+        {
+            using (var context = new ZRContext())
+            {
+                context.Entry(product).State = System.Data.Entity.EntityState.Modified;
+                context.SaveChanges();
+            }
+        }
+        public void DeleteProduct(int ID)
+        {
+            using (var context = new ZRContext())
+            {
+                var product = context.Products.Find(ID);
+                context.Products.Remove(product);
+                context.SaveChanges();
+            }
+        }
+
+
+    }
+}
---
+++ ZeerabCollection.Web/Controllers/ProductController.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using ZeerabCollection.Entities;
+using ZeerabCollection.Services;
+
+namespace ZeerabCollection.Web.Controllers
+{
+    public class ProductController : Controller
+    {
+        ProductsService productsService = new ProductsService();
+        // GET: Product
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult ProductTable(string search)
+        {
+            var products = productsService.GetProducts();
+            if (string.IsNullOrEmpty(search) == false)
+            {
+                products = products.Where(p => p.Name!=null && p.Name.ToLower().Contains(search.ToLower())).ToList();
+            }
+           
+            return PartialView(products);
+        }
+        //This method Show something to user or it shows the information that user want to see 
+        [HttpGet]
+        public ActionResult Create()
+        {
+            return PartialView();
+        }
+        //This method use when a user send information to database  
+        [HttpPost]
+        public ActionResult Create(Product product)
+        {
+
+            productsService.SaveProduct(product);
+            return RedirectToAction("Index");
+        }
+    }
+}
